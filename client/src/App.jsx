1d3502aaf1f2408fc91bddceec6bf9ba132041fe
Abhinav rana <abhinav.rana1016@gmail.com>
2024-05-06 10:48:43 +0530

    header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
+import React, { lazy } from 'react'
 import { BrowserRouter,Route,Routes } from 'react-router-dom'
-import Home from "./pages/Home"
-import About from './pages/About'
-import SignIn from './pages/SignIn'
-import SignUp from './pages/SignUp'
-import Profile from './pages/Profile'
+import Header from './Component/Header'
+const Home = lazy(()=>import("./pages/Home"))
+const About = lazy(()=>import("./pages/About"))
+const SignIn = lazy(()=>import("./pages/SignIn"))
+const SignUp = lazy(()=>import("./pages/SignUp"))
+const Profile = lazy(()=>import("./pages/Profile"))
 const App = () => {
   return (
    <BrowserRouter>
+   <Header/>
    <Routes>
    <Route path='/' element={<Home/>}/>
    <Route path='/about' element={<About/>}/>
