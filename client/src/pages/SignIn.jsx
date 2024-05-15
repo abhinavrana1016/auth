0fc9ade526c4d8cc47739235447a0d359fa53091
Abhinav rana <abhinav.rana1016@gmail.com>
2024-05-15 09:07:52 +0530

    sign in

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
-
+import React from "react";
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
 const SignIn = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const checkLogin = async (data) => {
+    try {
+      console.log(data);
+    } catch (error) {}
+  };
   return (
-    <div>SignIn</div>
-  )
-}
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Sign In</h1>
+        <form onSubmit={handleSubmit(checkLogin)}   className="flex flex-col gap-4">
+            <label className="form-label">Email</label>
+            <input
+              type="email"
+              className="bg-slate-100 p-3 rounded-lg"
+              {...register("email", { required: true })}
+            ></input>
+            <label>password</label>
+            <input
+              type="password"
+              className="bg-slate-100 p-3 rounded-lg"
+              {...register("password", { required: true })}
+            ></input>
+           <button type="submit" className="rounded-md border-black border-2 ">
+          Click me to Sign In
+        </button>
+         
+        </form>
+        <div className="flex gap-2 mt-5">
+        <p>Don't have an Account ?</p>
+        <Link to ='/sign-up' className="text-blue-600">Click to Register</Link>
+        </div>
+    
+    </div>
+  );
+};
 
-export default SignIn
+export default SignIn;
